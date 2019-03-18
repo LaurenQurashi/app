d8e21e1fee74eacbebca7e021d722fd82d5622d0
Lauren <laurenqurashi@me.com>
2019-03-18 16:26:30 +0000

    rebuilt the names feature with params

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ get '/webs' do
   "webbo"
 end
 
-get '/cat' do #/cat is the extention on our home directory.
-  @name = ["Keith", "Ian", "Greg"].sample # some code that randomises our stuff. 
+get '/random-cat' do #/cat is the extention on our home directory.
+  @name = ["Keith", "Ian", "Greg"].sample # some code that randomises our stuff.
+  erb(:index) #seperates our CSS from our ruby.
+end
+
+get '/named-cat' do #/cat is the extention on our home directory.
+  p params
+  @name = params[:name] # some code that randomises our stuff.
   erb(:index) #seperates our CSS from our ruby.
 end
