23c402c4fc5e4c62609e5173c8893c52c19d12e3
Lauren <laurenqurashi@me.com>
2019-03-18 16:07:33 +0000

    Changed the cat names and refactored to keep my code in the right file

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 
-get '/' do # this is our main webpage, our homepage if you will. 
+get '/' do # this is our main webpage, our homepage if you will.
   "Hello World"
 end
 
@@ -13,5 +13,6 @@ get '/webs' do
 end
 
 get '/cat' do #/cat is the extention on our home directory.
+  @name = ["Keith", "Ian", "Greg"].sample # some code that randomises our stuff. 
   erb(:index) #seperates our CSS from our ruby.
 end
